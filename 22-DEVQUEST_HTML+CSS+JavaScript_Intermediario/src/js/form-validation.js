bd863243ea06eea7c6d4e1c1f2e4e824fccd62b7
Guilherme Ribeiro de Souza <91293963+GuilhermeRS92@users.noreply.github.com>
2023-01-18 19:18:09 -0300

    correcao DEVQUEST 22

--- 22-DEVQUEST_HTML+CSS+JavaScript_Intermediario/src/js/form-validation.js
+++ 22-DEVQUEST_HTML+CSS+JavaScript_Intermediario/src/js/form-validation.js
@@ -4,16 +4,13 @@ const informacoes = document.querySelectorAll('.campo')
 botaoEnviar.addEventListener('click', function(){
     informacoes.forEach(item =>{
         if(item.value !== ""){
-            console.log(true)
             item.classList.remove('erroCampoObrigatorio')
             item.classList.add('campoPreenchido')
             item.nextElementSibling.classList.add('textoSemErroCampoObrigatorio')
         } else{
-            console.log(false)
             item.classList.remove('campoPreenchido')
             item.classList.add('erroCampoObrigatorio')
             item.nextElementSibling.classList.remove('textoSemErroCampoObrigatorio')
-            console.log(item.parentElement)
         }
     })
 })
